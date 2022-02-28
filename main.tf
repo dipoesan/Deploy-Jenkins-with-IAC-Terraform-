56070e851860135c45f40f690fe98636fdb830f6
dipoesan <esan.dipo@gmail.com>
2022-02-28 20:48:12 +0100

    Added SG and allowed ports in main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "dipo_jenkins_sg" {
+    name        = "dipo_jenkins_sg"
+    description = "Allow traffic to the Jenkins server"
+    vpc_id      = var.vpc_id
+
+    ingress {
+    description      = "Allow from Personal CIDR block"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = [var.cidr_block]
+    }
+
+    ingress {
+    description      = "Allow SSH from Personal CIDR block"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.cidr_block]
+    }
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    }
